6022391f83e9f0f2d3dfcd4c63e671591085d977
Rafael Paz <rafaelpma@gmail.com>
2022-04-23 12:04:40 -0300

    Commit inicial

---
+++ src/main/java/com/example/professor/allocation/rafael/ProfessorAllocationRafaelApplication.java
@@ -0,0 +1,13 @@
+package com.example.professor.allocation.rafael;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationRafaelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationRafaelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/professor/allocation/rafael/ProfessorAllocationRafaelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.professor.allocation.rafael;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationRafaelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
