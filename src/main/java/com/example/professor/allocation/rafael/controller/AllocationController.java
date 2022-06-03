3b9949c08742fc5f4f1cf420900dbfe983ee05db
Rafael Paz <rafaelpma@gmail.com>
2022-06-03 20:59:36 -0300

    Correcao do import

--- src/main/java/com/example/professor/allocation/rafael/controller/AllocationController.java
+++ src/main/java/com/example/professor/allocation/rafael/controller/AllocationController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ import com.example.professor.allocation.rafael.service.AllocationService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 @RestController
 @RequestMapping(path = "/allocations")
